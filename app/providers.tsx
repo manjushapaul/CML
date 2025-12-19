205657aae32b9ae3f57a5c7f431381ae2536fe64
Manjusha Paul <manjushapaul39@gmail.com>
2025-12-19 15:09:41 +0530

    Fix TypeScript error: remove unused next-themes import

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { ThemeProvider } from 'next-themes'
 import { useEffect } from 'react'
 import Lenis from 'lenis'
 
@@ -30,9 +29,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
   }, [])
 
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+    <>
       {children}
-    </ThemeProvider>
+    </>
   )
 }
 
